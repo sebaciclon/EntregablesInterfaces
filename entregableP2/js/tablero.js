0c97c01fd36cbe30a6b46bd3e2fafcd6dd68ada3
seba <sebaesains77@gmail.com>
2021-10-13 11:51:11 -0300

    Se modifico ganador horizontal

--- entregableP2/js/tablero.js
+++ entregableP2/js/tablero.js
@@ -129,7 +129,7 @@ class Tablero{
                         contJug = 0;
                     }
                 //}
-                contJug = 0;
+                //contJug = 0;
             }
             contJug = 0;
         }
