c1e2a606b3f31d326a9f1ec79dc0d9fef237118c
seba <sebaesains77@gmail.com>
2021-10-26 17:59:44 -0300

    Se agregaron comentarios a metodos

--- entregableP3/js/main.js
+++ entregableP3/js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-//VARIABLE USADA PARA CAMBIAR CLASES ENTRE SALTAR, HACERSE CHIQUITO Y CAMINAR
+//VARIABLES USADA PARA CAMBIAR CLASES ENTRE SALTAR, HACERSE CHIQUITO Y CAMINAR
 let salta = false;
 let chiquito = false;
 let caminaOSalta = document.getElementById("camina");
@@ -14,8 +14,7 @@ let puntosTotal = document.getElementById("puntos");
 let vidas = 3;
 let vidasTotal = document.getElementById("vidas");
 
-
-
+//VARIABLES USADAS PARA CAMBIAR EL ESCENARIO, YA SEA NOCHE O DIA
 let fondo = document.getElementById("contenedor");
 let sol = document.getElementById("sol");
 
@@ -40,6 +39,7 @@ function fondoDia() {
 }
 
 //METODO QUE DISPARA LA FUNCION PARA QUE EL NENE SALTE CUANDO SE PRECIONA LA BARRA ESPACIADORA
+//O SE HAGA CHIQUITO CUANDO SE PRECIONA LA FLECHA PARA ABAJO
 document.addEventListener('keydown', function(evento) {
     if(evento.keyCode == 32) { //barra espaciadora
         salta = true;
@@ -53,8 +53,10 @@ document.addEventListener('keydown', function(evento) {
     }
 });
 
-//METODO QUE SE UTILIZA PARA QUE EL NENE SALTE O CAMINE
-//SI LA VARIABLE SALTA ES TRUE SE SETEA LA CLASE SALTA, Y SI ES FALSE SE SETEA LA CLASE CAMINA
+//METODO QUE SE UTILIZA PARA QUE EL NENE SALTE, CAMINE O SE HAGA CHIQUITO
+//SI LA VARIABLE SALTA ES TRUE SE SETEA LA CLASE SALTA Y EL PERSONAJE SALTA,
+//SI LA VARIABLE CHIQUITO ES TRUE SE SETEA LA CLASE CHIQUITO Y EL PERSONAJE SE ENCOJE,
+//Y SI LAS DOS SON FALSE EL PERSONAJE CAMINA
 function cambiar(){
     if(salta)
         caminaOSalta.setAttribute("class","salta");
@@ -65,39 +67,18 @@ function cambiar(){
             caminaOSalta.setAttribute("class","camina");
 }
 
-//
+//SI EL NENE SALTA O SE HIZO CHIQUITO, SE VUELVEN LAS VARIABLES SALTA Y CHIQUITO A FALSE
+//Y SE LLAMA LA FUNCION CAMBIAR() PARA QUE SIGA CAMINANDO
 caminaOSalta.addEventListener("animationend", function() {
     salta = false;
     chiquito = false;
     cambiar();
 });
 
-/*function pierde(){ 
-    let posicionCaminar = camina.getBoundingClientRect();
-    let posicionPiedra = piedra.getBoundingClientRect();
-    let posicionToma = tomaDeAgua.getBoundingClientRect();
-    
-    let piedraW = posicionPiedra.left + posicionPiedra.width; 
-    let piedraH = posicionPiedra.top + posicionPiedra.height;
-    let caminaW = posicionCaminar.left + posicionCaminar.width -50;
-    let caminaH = posicionCaminar.top + posicionCaminar.height - 50;
-    let tomaAguaW = posicionToma.left + posicionToma.width;
-    let tomaAguaH = posicionToma.top + posicionToma.height;
-    
-    if((posicionCaminar.left <= piedraW  && posicionCaminar.top <= piedraH && caminaW >= posicionPiedra.left && caminaH >= posicionPiedra.top) 
-        || (posicionCaminar.left <= tomaAguaW  && posicionCaminar.top <= tomaAguaH && caminaW >= posicionToma.left && caminaH >= posicionToma.top)) {
-            camina.setAttribute("class","muere");
-            //restarVidas();
-            if(restarVidas() == 0) {
-                //hacer algo
-            }
-            //vidasTotal.innerHTML = vidas;
-            //vidas --;
-    }
-
-    
-}*/
-
+//METODO QUE SE FIJA SI EL NENE SE CHOCA UNA PIEDRA PARA RESTARLE UNA VIDA
+//SE OBTIENEN LAS POSCIONES DE EL NENE Y LA PIEDRA Y SE LAS COMPARA,
+//SI LAS POSICIONES COINCIDEN SIGNIFICA QUE EL NENE SE CHOCO UNA PIEDRA,
+//Y SINO SIGNIFICA QUE LA SALTO O AUN NO ESTA LA PIEDRA EN SU POSICION
 //restar pixeles al muñeco
 function pierdePiedra(){ 
     let posicionCaminar = camina.getBoundingClientRect();
@@ -113,10 +94,17 @@ function pierdePiedra(){
         restarVidas();
         if(vidas == 0) {
             //hacer algo
+            //setTimeout(gameOver(),3000);
+            detenerAnimaciones();
+            swal('GAME OVER ', 'Perdió todos sus vidas!!!', 'success');
         }
     }
 }
 
+//METODO QUE SE FIJA SI EL NENE SE CHOCA EL PAJARO PARA RESTARLE UNA VIDA
+//SE OBTIENEN LAS POSCIONES DE EL NENE Y EL PAJARO Y SE LAS COMPARA,
+//SI LAS POSICIONES COINCIDEN SIGNIFICA QUE EL NENE SE CHOCO EL PAJARO,
+//Y SINO SIGNIFICA QUE SE HIZO CHIQUITO O AUN NO ESTA EL PAJARO EN SU POSICION
 function pierdePajaro(){ 
     let posicionCaminar = camina.getBoundingClientRect();
     let posicionPajaro = pajaro.getBoundingClientRect();
@@ -124,46 +112,29 @@ function pierdePajaro(){
     let caminaW = posicionCaminar.left + posicionCaminar.width -50;
     let caminaH = posicionCaminar.top + posicionCaminar.height - 50;
     let pajaroW = posicionPajaro.left + posicionPajaro.width;
-    let pajaroH = posicionPajaro.top + posicionPajaro.height;
     
-    //if(posicionCaminar.left <= pajaroW  && posicionCaminar.top <= pajaroH && caminaW >= posicionPajaro.left && caminaH >= posicionPajaro.top) {
     if(posicionCaminar.left <= pajaroW  && posicionCaminar.top <= posicionPajaro.top && caminaW >= posicionPajaro.left && caminaH >= posicionPajaro.top) {
-        
         restarVidas();
         camina.setAttribute("class","muere");
-        if(vidas == -1) {
-            setTimeout(gameOver(),3000);
+        if(vidas == 0) {
+            //setTimeout(gameOver(),3000);
+            detenerAnimaciones();
+            swal('GAME OVER ', 'Perdió todos sus vidas!!!', 'success');
         }
-
+        
     }
 }
 
 function gameOver(){
     camina.setAttribute("class","muere");
-    alert("GAME OVER", "Perdió todos sus vidas");
+    //pausarAnimaciones();
+    swal('GAME OVER ', 'Perdió todos sus vidas!!!', 'success');
 }
 
-/*function pierdeToma(){ 
-    let posicionCaminar = camina.getBoundingClientRect();
-    let posicionToma = tomaDeAgua.getBoundingClientRect();
-    
-    let caminaW = posicionCaminar.left + posicionCaminar.width -50;
-    let caminaH = posicionCaminar.top + posicionCaminar.height - 50;
-    let tomaAguaW = posicionToma.left + posicionToma.width;
-    let tomaAguaH = posicionToma.top + posicionToma.height;
-    
-    if(posicionCaminar.left <= tomaAguaW  && posicionCaminar.top <= tomaAguaH && caminaW >= posicionToma.left && caminaH >= posicionToma.top) {
-        camina.setAttribute("class","muere");
-        let cantVidas = restarVidas();
-        puntos = 0;
-        puntosTotal.innerHTML = puntos;
-        if(cantVidas == 0) {
-            //hacer algo
-        }
-
-    }
-}*/
-
+//METODO QUE SE FIJA SI EL NENE "AGARRA" LA MONEDA MAS ALTA PARA SUMAR PUNTOS
+//SE OBTIENEN LAS POSCIONES DE EL NENE Y LA MONEDA MAS ALTA Y SE LAS COMPARA,
+//SI LAS POSICIONES COINCIDEN SIGNIFICA QUE EL NENE "AGARRO" LA MONEDA,
+//Y SINO SIGNIFICA QUE NO LA PUEDO AGARRAR
 function sumarPuntosArriba() {
     let posicionCaminar = camina.getBoundingClientRect();
     let posicicionMoneda = moneda.getBoundingClientRect();
@@ -171,33 +142,30 @@ function sumarPuntosArriba() {
     let caminaW = posicionCaminar.left + posicionCaminar.width;
     let caminaH = posicionCaminar.top + posicionCaminar.height;
     let monedaW = posicicionMoneda.left + posicicionMoneda.width; 
-    let monedaH = posicicionMoneda.top + posicicionMoneda.height;
     
-    //if(posicionCaminar.left <= monedaW  && posicionCaminar.top <= monedaH && caminaW >= posicicionMoneda.left && caminaH >= posicicionMoneda.top) {
     if(posicionCaminar.left <= monedaW  && posicionCaminar.top <= posicicionMoneda.top && caminaW >= posicicionMoneda.left && caminaH >= posicicionMoneda.top) {
        sumar();
-        //camina.setAttribute("class","muere");
         moneda.setAttribute("class","agarraMoneda");
         setTimeout(mostrarMonedaArriba, 2000);
-        
-
     }
-     
 }
 
+//METODO QUE MUESTRA LA CLASE MONEDA, EN ESTE CASO LA MONEDA MAS ALTA
 function mostrarMonedaArriba(){
     moneda.setAttribute("class","moneda");
 }
 
+//METODO QUE SE FIJA SI EL NENE "AGARRA" LA MONEDA MAS BAJA PARA SUMAR PUNTOS
+//SE OBTIENEN LAS POSCIONES DE EL NENE Y LA MONEDA MAS BAJA Y SE LAS COMPARA,
+//SI LAS POSICIONES COINCIDEN SIGNIFICA QUE EL NENE "AGARRO" LA MONEDA,
+//Y SINO SIGNIFICA QUE NO LA PUEDO AGARRAR
 function sumarPuntosAbajo() {
     let posicionCaminar = camina.getBoundingClientRect();
     let posicicionMoneda1 = moneda1.getBoundingClientRect();
     
-
     let caminaW = posicionCaminar.left + posicionCaminar.width;
     let caminaH = posicionCaminar.top + posicionCaminar.height;
     let moneda1W = posicicionMoneda1.left + posicicionMoneda1.width; 
-    let moneda1H = posicicionMoneda1.top + posicicionMoneda1.height;
     
     if(posicionCaminar.left <= moneda1W  && posicionCaminar.top <= posicicionMoneda1.top && caminaW >= posicicionMoneda1.left && caminaH >= posicicionMoneda1.top) {
         sumar();
@@ -205,81 +173,50 @@ function sumarPuntosAbajo() {
         setTimeout(mostrarMonedaAbajo, 2000);
     }
 }
-  
+
+//METODO QUE MUESTRA LA CLASE MONEDA, EN ESTE CASO LA MONEDA MAS BAJA
 function mostrarMonedaAbajo(){
     moneda1.setAttribute("class","moneda1");
 }
 
-
-/*function sumarPuntos() {
-    let posicionCaminar = camina.getBoundingClientRect();
-    let posicicionMoneda = moneda.getBoundingClientRect();
-    let posicicionMoneda1 = moneda1.getBoundingClientRect();
-    console.log(posicionCaminar);
-    console.log(posicicionMoneda);
-
-    let caminaW = posicionCaminar.left + posicionCaminar.width;
-    let caminaH = posicionCaminar.top + posicionCaminar.height;
-    let monedaW = posicicionMoneda.left + posicicionMoneda.width; 
-    let monedaH = posicicionMoneda.top + posicicionMoneda.height;
-    let moneda1W = posicicionMoneda1.left + posicicionMoneda1.width; 
-    let moneda1H = posicicionMoneda1.top + posicicionMoneda1.height;
-    
-
-    //if(posicionCaminar.left <= monedaW  && posicionCaminar.top <= monedaH && caminaW >= posicicionMoneda.left && caminaH >= posicicionMoneda.top) {
-    if(posicionCaminar.left <= monedaW  && posicionCaminar.top <= posicicionMoneda.top && caminaW >= posicicionMoneda.left && caminaH >= posicicionMoneda.top) {
-       sumar();
-        //camina.setAttribute("class","muere");
-    }
-    if(posicionCaminar.left <= moneda1W  && posicionCaminar.top <= posicicionMoneda1.top && caminaW >= posicicionMoneda1.left && caminaH >= posicicionMoneda1.top) {
-        sumar();
-    
-    }
-}*/
-
+//METODO QUE SUMA 10 PUNTOS Y LO MUESTRA EN PANTALLA
 function sumar() {
     puntos += 10;
     puntosTotal.innerHTML = puntos;
 }
 
+//METODO QUE RESTA UNA VIDA Y LO MUESTRA EN PANTALLA
 function restarVidas() {
     vidas --;
     vidasTotal.innerHTML = vidas;
 }
-/*
-function inicioJuego(){
-    
+
+function detenerAnimaciones(){
     nubes.style.animationPlayState = "paused";
     sol.style.animationPlayState = "paused";
     ciudad.style.animationPlayState = "paused";
     camina.style.animationPlayState = "paused";
-    camina.classList.remove("camina");
-    //camina.classList.remove("muere");
     moneda.style.animationPlayState = "paused";
-    moneda.classList.remove("moneda");
     moneda1.style.animationPlayState = "paused"; 
-    moneda1.classList.remove("moneda1");
     piedra.style.animationPlayState = "paused"; 
-    piedra.classList.remove("piedra");
     pajaro.style.animationPlayState = "paused"; 
-    pajaro.classList.remove("pajaro");
-    
-   
 }
-inicioJuego(); */
-
-setInterval(pierdePiedra,250);
-//setInterval(pierdeToma,1000);
-setInterval(pierdePajaro, 1000);
-setInterval(sumarPuntosArriba, 500);
-setInterval(sumarPuntosAbajo, 500);
-
 
 /*setInterval(pierdePiedra,250);
-//setInterval(pierdeToma,1000);
+
 setInterval(pierdePajaro, 500);
 setInterval(sumarPuntosArriba, 500);
 setInterval(sumarPuntosAbajo, 500);*/
+
+function inicioJuego() {
+    setInterval(pierdePiedra,250);
+    setInterval(pierdePajaro, 500);
+    setInterval(sumarPuntosArriba, 500);
+    setInterval(sumarPuntosAbajo, 500);
+}
+
+inicioJuego();
+
 
 
 
